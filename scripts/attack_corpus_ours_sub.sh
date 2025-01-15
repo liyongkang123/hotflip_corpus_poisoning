f70c9e2b2c59f79b7fcc42a3574a2fd92c61c202
Li Yongkang <2859306808@qq.com>
2025-01-15 21:10:08 +0100

    fix typo

--- scripts/attack_corpus_ours_sub.sh
+++ scripts/attack_corpus_ours_sub.sh
@@ -16,7 +16,7 @@ sub_attack_rate=$4
 
 echo "Executing: python hptflip_ours_attack_corpus.py --attack_dataset ${sub_data} --attack_model_code ${sub_model} --split train --max_seq_length 128 --max_query_length 32 --num_cand 100  --num_iter 5000 --kmeans_split  --per_gpu_eval_batch_size 256 --init_gold True"
 
-python hptflip_ours_attack_corpus.py \
+python hotflip_ours_attack_corpus.py \
      --attack_dataset  ${sub_data} \
      --attack_model_code  ${sub_model} \
      --split test \
